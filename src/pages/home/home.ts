c412c0f36f38e7a2657d4c5c247e6f3840f1995e
James <james@nml.co.za>
2018-03-03 14:07:08 +0200

    YAY

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,19 +2,25 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 
-@Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
-})
+interface User {
+  id: string;
+  name: string;
+}
+
+@Component({selector: 'page-home', templateUrl: 'home.html'})
 export class HomePage {
 
+  users: User[] = [];
+
   constructor(public navCtrl: NavController, public afs:AngularFirestore) {
+    
     let collection = this.afs.collection('users');
     
-    collection.doc("avIyq6la855aKTuikuvm").ref.get().then((documentSnapshot) => 
-    { 
-      console.log("users", documentSnapshot.data());
-    }); //ref()
+    collection.ref.get().then(querySnapshot => {
+      querySnapshot.forEach(queryDocumentSnapshot => {
+        this.users.push({id: queryDocumentSnapshot.id, name: queryDocumentSnapshot.data().name});
+      });
+    });
   }
 
   ionViewWillEnter() {
