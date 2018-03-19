9364b401542befc4d1869a04b17f5265898309b6
Marcel Prinsloo <Marcel>
2018-03-19 22:27:13 +0200

    Item add and item delete button

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -7,17 +7,30 @@ import { Item } from '../../models/item';
 @Component({ selector: 'page-list', templateUrl: 'list.html' })
 export class ListPage {
 
-    list: List;
-    items: Item[] = [];
+    list : List;
+    items : Item[] = [];
 
-    constructor(private navParams : NavParams, private firestoreService : FirestoreService)
-    {
+    newItem : string;
+
+    constructor(private navParams : NavParams, private firestoreService : FirestoreService) {
         this.list = navParams.get('list');
         this.items = this.list.Items;
     }
 
-    itemCheck(itemId: string) {
+    public itemCheck(itemId : string) {
         var item = this.items.find(i => i.Id == itemId);
         item.State.checked = !item.State.checked;
+    }
+
+    public onBlur() {
+        console.log(`Triggered on blur with newItem value: '${this.newItem}'`);
+        if (this.newItem) {
+            this.items.push({ Id: "newId", State: { checked: false }, Text: this.newItem });
+            this.newItem = null;
+        }
+    }
+
+    public delete(item : Item) {
+        console.log(`Deleting item: ${item.Text}`);
     }
 }
