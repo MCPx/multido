a0e91722b4136fad45cd6be8ea9f9f8f726de0d1
Marcel Prinsloo <marcelcprinsloo@gmail.com>
2019-07-15 22:00:48 +0200

    Delete firestoreCredentials.ts

--- src/app/firestoreCredentials.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: 'AIzaSyCrciTg25RREKcnKB9z7Uwu9U1sWIG2cVU',
-    authDomain: 'multido-4f75f.firebaseapp.com',
-    databaseURL: 'https://multido-4f75f.firebaseio.com',
-    projectId: 'multido-4f75f',
-    storageBucket: 'multido-4f75f.appspot.com',
-    messagingSenderId: '793639388905',
-    appId: '1:793639388905:web:cd06d558c08ccf9d'
-};
