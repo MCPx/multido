a9d28463b545257f645fcd2eeab1d85dfedd3757
Marcel Prinsloo <marcelcprinsloo@gmail.com>
2019-07-16 22:00:56 +0200

    fix(list): Port list page.

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActionSheetController, AlertController, NavParams } from '@ionic/angular';
+import { FirestoreListService } from 'services/firestoreList.service';
+import { SiteStore } from 'services/siteStore';
+import { List } from 'models/list';
+import { Item } from 'models/item';
+import { uuid } from 'util/utility';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
     selector: 'app-list',
@@ -6,36 +15,154 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['list.page.scss']
 })
 export class ListPage implements OnInit {
-    private selectedItem: any;
-    private icons = [
-        'flask',
-        'wifi',
-        'beer',
-        'football',
-        'basketball',
-        'paper-plane',
-        'american-football',
-        'boat',
-        'bluetooth',
-        'build'
-    ];
-    public items: Array<{ title: string; note: string; icon: string }> = [];
 
-    constructor() {
-        for (let i = 1; i < 11; i++) {
-            this.items.push({
-                title: 'Item ' + i,
-                note: 'This is item #' + i,
-                icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-            });
-        }
+    list: List;
+    checkedItems: Item[];
+    uncheckedItems: Item[];
+    isUpdating = false;
+    newItemName: string;
+    debounceUpdate: Subject<void> = new Subject();
+
+    constructor(
+        private navParams: NavParams,
+        private listService: FirestoreListService,
+        private store: SiteStore,
+        private router: Router,
+        private alertCtrl: AlertController,
+        private actionSheetCtrl: ActionSheetController) {
+
+        this.list = this.navParams.get('list');
+        this.checkedItems = this.list.items.filter(x => x.state.checked);
+        this.uncheckedItems = this.list.items.filter(x => !x.state.checked);
     }
 
     ngOnInit() {
+        // adding list to firestore is triggered from DashboardPage - merge response with local when it completes
+        const addListPromise = this.navParams.get('addListPromise');
+        if (addListPromise) {
+            addListPromise.then(addedList => {
+                const items = this.list.items.concat(addedList.items);
+                this.list = addedList;
+                this.list.items = items;
+
+                this.checkedItems = this.list.items.filter(x => x.state.checked);
+                this.uncheckedItems = this.list.items.filter(x => !x.state.checked);
+
+                this.debounceUpdate.next();
+            });
+        }
+
+        this.debounceUpdate
+            .pipe(debounceTime(500))
+            .subscribe({ next: () => this.updateList() });
     }
 
-    // add back when alpha.4 is out
-    // navigate(item) {
-    //   this.router.navigate(['/list', JSON.stringify(item)]);
-    // }
+    ionViewWillEnter() {
+        this.debounceUpdate.next();
+    }
+
+    ionViewWillLeave() {
+        this.updateList();
+    }
+
+    // reorder on check
+    private handleItemCheck(item: Item) {
+        this.sort(item);
+    }
+
+    private sort(item: Item) {
+        this.uncheckedItems = this.list.items.filter(x => !x.state.checked);
+        this.checkedItems = this.list.items.filter(x => x.state.checked);
+
+        this.list.items = this.uncheckedItems.concat(this.checkedItems);
+
+        this.debounceUpdate.next();
+    }
+
+    private onBlur() {
+        if (this.newItemName) {
+            const newItem = new Item({ id: uuid(), state: { checked: false }, text: this.newItemName });
+            this.list.items.push(newItem);
+            this.sort(newItem);
+            this.newItemName = null;
+        }
+    }
+
+    async handleManageListClick() {
+        return this.router.navigate(['/manageList'], { queryParams: {
+            knownUserEmails: this.store.getUser().knownUserEmails,
+            list: this.list
+        }});
+    }
+
+    async presentActionSheet(item: Item) {
+        const actionSheet = await this.actionSheetCtrl.create({
+            header: 'Edit item',
+            buttons: [
+                {
+                    text: 'Delete',
+                    role: 'destructive',
+                    handler: () => {
+                        this.deleteItem(item);
+                    }
+                },
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    handler: () => {
+                    }
+                }
+            ]
+        });
+        return actionSheet.present();
+    }
+
+    private deleteItem(item: Item) {
+        this.list.items = this.list.items.filter(i => i !== item);
+        this.debounceUpdate.next();
+    }
+
+    private updateItem(item: Item, value: string) {
+        if (item.text === value) {
+            return;
+        }
+
+        item.text = value;
+        this.debounceUpdate.next();
+    }
+
+    async updateList() {
+        this.isUpdating = true;
+        await this.listService.updateList(this.list);
+        this.isUpdating = false;
+    }
+
+    private refreshList(e) {
+        this.listService.getUpdatedList(this.list)
+            .then(list => {
+                this.list = list;
+            }).then(() => e.complete());
+    }
+
+    async handleListNameClick() {
+        const nameEditAlert = await this.alertCtrl.create({
+            header: 'Edit name',
+            inputs: [{
+                type: 'text',
+                value: this.list.name,
+                name: 'name'
+            }],
+            buttons: [
+                'Cancel',
+                {
+                    text: 'Save',
+                    handler: data => {
+                        this.list.name = data.name;
+                        this.listService.updateList(this.list).then(() => this.isUpdating = false);
+                    }
+                }],
+        });
+
+        return nameEditAlert.present();
+    }
 }
